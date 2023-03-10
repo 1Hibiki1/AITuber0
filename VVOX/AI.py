a6d77b8113267d9dab880f33d6f26ca1e7859417
1Hibiki1 <43519087+1Hibiki1@users.noreply.github.com>
2023-03-10 12:50:58 -0500

    Cleanup (kinda)

--- VVOX/AI.py
+++ VVOX/AI.py
@@ -1,17 +1,29 @@
 import urllib
 import requests
-from playsound import playsound
 
+import platform
+cur_OS = platform.system()
+
+# import playsound if not on windows
+if cur_OS == "Windows":
+    import winsound
+else:
+    from playsound import playsound
+
+# VOICEVOX engine server url
 base_url = "http://localhost:50021"
+
+# audio file to save generated voice in
 speech_filename = "sp.wav"
-speaker_id = '6'
+
+# voice bank ID
+speaker_id = '1'
 
 def speak(sentence):
     # generate initial query
     params_encoded = urllib.parse.urlencode ({'text': sentence, 'speaker': speaker_id})
 
     r = requests.post (f'{base_url}/audio_query?{params_encoded}')
-
     voicevox_query = r.json ()
     voicevox_query['volumeScale'] = 4.0
     voicevox_query['intonationScale'] = 1.5
@@ -26,17 +38,24 @@ def speak(sentence):
         outfile.write(r.content)
 
     # play audio file
-    playsound(speech_filename)
+    if cur_OS == "Windows":
+        winsound.PlaySound(speech_filename, winsound.SND_FILENAME)
+    else:
+        playsound(speech_filename)
 
-# while 1:
-#     t = input("tts> ")
 
-#     if t == 'q':
-#         break
+if __name__ == '__main__':
+    print("type 'q' to exit! :)\n")
     
-#     if t.strip() == "chsp":
-#         new_sp = input("enter new speaker> ")
-#         speaker_id = new_sp.strip()
-#         continue
+    while 1:
+        t = input("tts> ")
 
-#     speak(t)
+        if t == 'q':
+            break
+        
+        if t.strip() == "chsp":
+            new_sp = input("enter new speaker> ")
+            speaker_id = new_sp.strip()
+            continue
+
+        speak(t)
--- charai/index.js
+++ charai/index.js
@@ -1,26 +1,44 @@
+/* character AI API */
 const CharacterAI = require('node_characterai');
 const characterAI = new CharacterAI();
 
+/* for user input */
 const prompt = require('prompt-sync')();
+
+/* change this for diferent character */
+const characterId = "yVso4c2Co_gZNovdIdqu3efLNJ_n6U2R5PXiMF44tbc";
+
+/* tts server params */
+const tts_server_base_url = "http://127.0.0.1";
+const tts_server_port = "6969";
+const tts_server_route = "/";
 
 (async() => {
     await characterAI.authenticateAsGuest();
-
-    // const characterId = "8_1NyR8w1dOXmI1uWaieQcd147hecbdIK7CeEAIrdJw" // Discord moderator
-    const characterId = "yVso4c2Co_gZNovdIdqu3efLNJ_n6U2R5PXiMF44tbc" // Discord moderator
-
     const chat = await characterAI.createOrContinueChat(characterId);
+
+    console.log("type 'q' to exit :)");
 
     while(1) {
         const msg = prompt('> ');
+
+        /* quit if user enters 'q' */
         if(msg == 'q')
             break;
+
+        /* get message response */
         const response = await chat.sendAndAwaitResponse(msg, true);
+
+        /* print response */
+        console.log("\n");
         console.log(response.text);
-        await fetch('http://localhost:6969/', {
+        console.log("\n");
+
+        /* send message to tts server */
+        await fetch(`${tts_server_base_url}:${tts_server_port}${tts_server_route}`, {
             method: 'POST',
             body: response.text
-        })
+        });
     }
 
 })();
